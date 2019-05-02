b44d95397552b71a742a3e84b9547e85e71f5ac7
yyyyyan <yanzhengqin1993@gmail.com>
2019-05-03 01:54:00 +0200

    1A.7 - Updated trip services to get cover photo object instead of src

--- services/trip/utils.js
+++ services/trip/utils.js
@@ -8,16 +8,16 @@ class Utils {
             users: trip.users,
             beginDate: trip.dailyInfos[0].date,
             endDate: trip.dailyInfos[trip.dailyInfos.length-1].date,
-            coverPhotoId: trip.coverPhotoId,
-            coverPhotoSrc: trip.coverPhotoSrc
+            coverPhoto: trip.coverPhoto
         }
     }
-    generateDailyInfosWithCoverPhotoSrc(dailyInfos, photos) {
+    generateDailyInfosWithCoverPhotos(dailyInfos, photos) {
         // TODO find a better way to avoid triple loops
         dailyInfos = dailyInfos.map(dailyInfo => {
             dailyInfo.locs = dailyInfo.locs.map(loc => {
                 loc.covers = loc.covers.map(cover => {
-                    cover.photoSrc = this.generateCompletePhotoSrc(photos[cover.photoId].src)
+                    cover.photo = photos[cover.photoId]
+                    cover.photo.src = this.generateCompletePhotoSrc(cover.photo.src)
                     return cover
                 })
                 return loc
