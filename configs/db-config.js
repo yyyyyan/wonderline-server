70ddfaf8f1f77534c5375bb1d1bb2ba01f126780
Yan <yanzhengqin1993@gmail.com>
2019-11-09 14:04:45 +0100

    1A.17 Added icons/photos for Jon Snow profile. Added profileBkgSrc, signature in User and updated logic. (#2)

--- configs/db-config.js
+++ configs/db-config.js
@@ -2,5 +2,6 @@ const path = require('path')
 export default {
     DB_ROOT_PATH: path.join(__dirname, '../database'),
     MEDIA_ROOT_PATH: path.join(__dirname, '../public'),
-    DEFAULT_AVATAR_REL_PATH: '/assets/default_avatar.png'
+    DEFAULT_AVATAR_REL_PATH: '/assets/default_avatar.png',
+    DEFAULT_PROFILE_BKG_REL_PATH: '/assets/default_profile_bkg.jpeg',
 }
