027d6c63ee9b44b8dc243edf5eedd930e2db6a91
Zhe ZHOU <kiscg@zhes-macbook-pro.home>
2020-02-22 18:57:22 +0100

    Hotfix - Updated ip addr to adapt new local environment.

--- configs/http-config.js
+++ configs/http-config.js
@@ -1,6 +1,6 @@
 export default {
     SERVER_PORT: '3001',
-    SERVER_ADDRESS: 'http://192.168.1.25:3001',
+    SERVER_ADDRESS: 'http://192.168.1.83:3001',
     GET_SUCCESS_CODE: '200',
     POST_SUCCESS_CODE: '201',
     MAX_FILE_NB_PER_POST: 12
